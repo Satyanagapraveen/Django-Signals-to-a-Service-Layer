100be33656db7a025374736be4a419cd279a5efc
SATYA NAGA PRAVEEN KUMAR NAMBURI <153361064+Satyanagapraveen@users.noreply.github.com>
2026-08-08 16:59:27 +0530

    Implement signals to update user statistics on order creation

---
+++ orders/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Order,UserStats
+
+@receiver(post_save, sender=Order)
+def update_user_stats_on_order_save(sender, instance, created,**kwargs):
+    if created:
+        user = instance.user
+        stats, _= UserStats.objects.get_or_create(user=user)
+
+        stats.order_count+=1
+        stats.total_spent+=instance.total
+        stats.save()
+        
